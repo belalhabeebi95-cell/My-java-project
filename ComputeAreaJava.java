2cfe1271910bc7fb8d762008f8297c914d8a5152
belalhabeebi95-cell <belalhabeebi95@gmail.com>
2025-11-12 12:34:38 +0430

    Add files via upload

---
+++ ComputeAreaJava.java
@@ -0,0 +1,21 @@
+
+package computearea.java;
+
+import java.util.Scanner;
+public class ComputeAreaJava {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number for radius");
+        double radius =input.nextDouble();
+        double area;
+        area = radius*radius*Math.PI;
+        System.out.println("The area for the circle of radius "+radius+" is "+area);
+        
+        
+        
+        
+      
+    }
+    
+}
